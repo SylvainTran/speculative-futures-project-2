2ffa76e742f476ea59e1ee56c854a8245f9bf41b
SylvainTran <45033804+SylvainTran@users.noreply.github.com>
2022-08-15 12:58:56 -0400

    Reduced the number of friends
    Abrupt decision. But this feels soooo much better now.

--- IntegrationTests/launcher/src/app/services/mockfriendlist.ts
+++ IntegrationTests/launcher/src/app/services/mockfriendlist.ts
@@ -4,7 +4,8 @@ export class MockFriendList {
     public friendList: Friend[] | undefined = [];
 
     constructor() {
-        let friendNames : string[] = ["TryAgain34", "_purgeMe_", "SylvainTran", "herohero", "Eden_Martrop", "Aingealag", "fallingheadfirst49", "BoboCannon", "Catherine", "D_Jeanne58"];
+        //let friendNames : string[] = ["TryAgain34", "_purgeMe_", "SylvainTran", "herohero", "Eden_Martrop", "Aingealag", "fallingheadfirst49", "BoboCannon", "Catherine", "D_Jeanne58"];
+        let friendNames : string[] = ["_purgeMe_", "SylvainTran", "herohero"];
         friendNames.forEach(name => { this.friendList?.push(new Friend(name, "online"))});
         this.friendList?.map(f => console.log(f.name));
     }
